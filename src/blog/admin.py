88f76399aa9e7f7c3cb5cc704e254e7165481aea
phablo-tavares <phablo.tavares1@gmail.com>
2023-11-16 15:37:31 -0300

    asdfasdf

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Autor,Post,Comentario
 
 # Register your models here.
+admin.site.register(Autor)
+admin.site.register(Post)
+admin.site.register(Comentario)
